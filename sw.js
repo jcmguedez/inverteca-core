fc8508dd1250841765debb1054d78e3b7337120c
jcmguedez <jcmguedez03@gmail.com>
2026-04-22 08:37:32 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME ).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', e => {
